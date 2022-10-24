bcf3b25554bd65f3ec86727cbae0d826aa3b3b41
Guillermo Della Rosa <guillesdr@gmail.com>
2022-10-24 00:09:24 -0300

    Fix Header an Menu

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import React, { Component } from 'react';
 import Header from './components/Header'
 import MapChart from './components/MapChart';
 import TableData from './components/TableData';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <>
       <Header></Header>
-      <MapChart></MapChart>
+      <Menu></Menu>
+      <div class="max-w-sm w-full lg:max-w-full lg:flex">
+        <MapChart></MapChart>
+      </div>
+
       <TableData></TableData>
     </>
   );
---
+++ src/components/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+<div class="flex justify-content: center">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Real Time
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Today
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Future
+        </button>
+</div>
+
+ 
+  )
+}
+
+export default Menu

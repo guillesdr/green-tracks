bcb7b39ca168874375f0e00a508d4353d057e14d
Guillermo Della Rosa <guillesdr@gmail.com>
2022-10-26 00:19:37 -0300

    Pages And graphs

---
+++ src/components/TitleDescriptions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TitleDescriptions = () => {
+  return (
+    <>
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Forecast</span></h1>
+    <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Future Estimations based in the workload duration (in minutes)</p>
+    <br></br>
+    </>
+  )
+}
+
+export default TitleDescriptions
